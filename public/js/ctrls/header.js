0b4c99f74e23fc7f3e3f0cb08118f8d1815b838c
Reitsuki Sion <kennyjsk@gmail.com>
2014-09-07 17:01:00 +0800

    头部控制器添加
    头部控制主负责设置当前导航栏和 显示导航栏那个连接是被激活的

---
+++ public/js/ctrls/header.js
@@ -0,0 +1,19 @@
+function header($scope,$route, $location){
+    // 测试数据,后面会由后台吐出
+    $scope.navs = {
+        "主页": {href: "#/", active: false},
+        "留言板": {href: "#/messageBoard", active: false},
+        "关于我": {href: "#/messageBoard", active: false}
+    };
+
+    // 设置当前的页面
+    var currPath = $location.path();
+    for(key in $scope.navs){
+        if($scope.navs[key].href == "#" + currPath){
+            $scope.navs[key].active = true;
+            console.log(key);
+        }else{
+            $scope.navs[key].active = false;
+        }
+    }
+}
