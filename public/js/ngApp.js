f32ea4feda296e0f1b62cb9672c755180fb44993
Reitsuki Sion <kennyjsk@gmail.com>
2014-09-07 16:38:46 +0800

    添加路由规则
    会根据是否后台 和 根据前台主题读取模板

--- public/js/ngApp.js
+++ public/js/ngApp.js
@@ -2,6 +2,21 @@
 // 定义APP模块
 window.app = angular.module("SionBlog", ["ngRoute"]);
 
+// 定义路由
+app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider){
+    $routeProvider.when("/",{
+            templateUrl: 'home.html',
+            controller: HomeCtrl,
+            inAdminPanel: false
+        })
+
+        // 默认页面
+        .otherwise('/',{
+            redirectTo: '/'
+        })
+}]);
+
+// 初始化 APP
 app.run(['$rootScope','$location','$http', '$route','$timeout', function($rootScope,$location,$http,$route,$timeout){
     // 加载完成 angular 后隐藏页面进度条 
     $rootScope.progress = 100;
@@ -11,5 +26,15 @@ app.run(['$rootScope','$location','$http', '$route','$timeout', function($rootSc
 
     //默认主题名字
     $rootScope.theme = "default";
-    
+
+    $rootScope.blogName = "xxxx's Blog";
+
+    // 根据主题加载响应的模板(路由变更开始事件)
+    $rootScope.$on("$routeChangeStart", function(event, next, current){
+        if(!next.$$route.inAdminPanel){
+            next.templateUrl = "/tpl/" +  $rootScope.theme + "/" + next.templateUrl;
+        }else{
+            next.templateUrl = "/tpl/admin/" + next.templateUrl;
+        }
+    });
 }]);
